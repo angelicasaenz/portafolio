09e4bde6820c6a797dd2576606d316e2e8a1afd3
Angélica Sáenz <114958387+angelicasaenz@users.noreply.github.com>
2026-08-09 16:33:02 -0500

    refactor: improve mobile menu

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const botonMenu = document.querySelector(".menu-hamburguesa");
 const menu = document.querySelector(".nav-menu");
 const overlay = document.querySelector(".overlay");
+const cerrarMenu = document.querySelector(".cerrar-menu");
 
 botonMenu.addEventListener("click", () => {
     menu.classList.toggle("activo");
     overlay.classList.toggle("activo");
+});
+
+cerrarMenu.addEventListener("click", () => {
+    menu.classList.remove("activo");
+    overlay.classList.remove("activo");
+}); 
+
+const enlacesMenu = document.querySelectorAll(".nav-menu a");
+
+enlacesMenu.forEach((enlace) => {
+    enlace.addEventListener("click", () => {
+        menu.classList.remove("activo");
+        overlay.classList.remove("activo");
+    });
 });
